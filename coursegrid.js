b7f93d3c263681d06d8864f768ade45fb1ff2d81
jolson615 <jolson615@gmail.com>
2018-03-21 17:22:36 -0400

    grid

---
+++ coursegrid.js
@@ -0,0 +1,39 @@
+console.log("script running")
+
+const wrapper = document.querySelector('#wrapperGrid')
+console.log(wrapper)
+
+let courses = []
+
+fetch("https://raw.githubusercontent.com/jolson615/JSCalendar/master/schedule.json")
+  .then(r => r.json())
+  .then(data => {
+    console.log(data)
+    courses = data
+    makeGrid()
+  }).catch(e => console.log("Error"))
+
+function makeGrid() {
+  courses.forEach(course => {
+    addCell(course)
+  })
+}
+
+function addCell(course) {
+  let start = course["Start Date"].slice(6).replace("-","/")
+  let end = course["End Date"].slice(6).replace("-","/")
+  let classname = course["Class Code"]
+  let session = course["Session"]
+  console.log( `
+  <div class="course ${classname} s${session}">
+    <h3 class="coursename">${classname}</h3>
+    <h4 class="coursedate">${start} - ${end}</h4>
+  </div>
+  `)
+  wrapper.innerHTML += `
+  <div class="course ${classname} s${session}">
+    <h3 class="coursename">${classname}</h3>
+    <h4 class="coursedate">${start} - ${end}</h4>
+  </div>
+  `
+}
